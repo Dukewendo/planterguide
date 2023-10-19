97d8a63ec8674f5d07caad729ec296eeaaad1976
Dukewendo <dommct@yahoo.co.uk>
2023-10-19 11:59:35 +0100

    simple header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import Header from './components/Header';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />  
       <h1>Uk Planting guide</h1>
     </div>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import '../components/header.css'
+
+export default function header() {
+  return (
+    <div className='header-container'>
+        <ul className='header-list'>
+            <li>Home</li>
+            <li>About</li>
+        </ul>
+    </div>
+  )
+}
