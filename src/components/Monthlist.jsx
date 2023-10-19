48b096ed34a57d5a6996e0eab4e9bae83c4f82fd
Dukewendo <dommct@yahoo.co.uk>
2023-10-19 16:26:47 +0100

    monthlist routes

--- src/components/Monthlist.jsx
+++ src/components/Monthlist.jsx
@@ -10,13 +10,48 @@ export default function Monthlist() {
     <HashRouter basename="/">
       <div className="monthlist-container">
         <ul>
-          <li>
-            <Link to="/planterguide">PlanterGuide Home</Link>
+          <li className="main-list">
+            <Link to="/planterguide">Home</Link>
+          </li>
+          <li className="main-month-list">
+            <Link to="/January">January</Link>
+          </li>
+          <li className="main-month-list">
+            <Link to="/February">February</Link>
+          </li>
+          <li className="main-month-list">
+            <Link to="/March">March</Link>
+          </li>
+          <li className="main-month-list">
+            <Link to="/April">April</Link>
+          </li>
+          <li className="main-month-list">
+            <Link to="/May">May</Link>
+          </li>
+          <li className="main-month-list">
+            <Link to="/June">June</Link>
+          </li>
+          <li className="main-month-list">
+            <Link to="/July">July</Link>
+          </li>
+          <li className="main-month-list">
+            <Link to="/August">August</Link>
+          </li>
+          <li className="main-month-list">
+            <Link to="/September">September</Link>
+          </li>
+          <li className="main-month-list">
+            <Link to="/October">October</Link>
+          </li>
+          <li className="main-month-list">
+            <Link to="/November">November</Link>
+          </li>
+          <li className="main-month-list">
+            <Link to="/December">December</Link>
           </li>
         </ul>
-
         <Routes>
-          <Route path="/planterguide" element={<Planterguide/>} />
+          <Route path="/planterguide" element={<Planterguide />} />
         </Routes>
       </div>
     </HashRouter>
