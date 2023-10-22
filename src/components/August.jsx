f84cb8d0e513f8c665feb16cd18b914ff2cbd75f
Dukewendo <dommct@yahoo.co.uk>
2023-10-22 23:08:46 +0100

    auguest content

--- src/components/August.jsx
+++ src/components/August.jsx
@@ -1,19 +1,64 @@
-import React from 'react'
+import React from "react";
 
 import "./monthstyles.css";
 
 export default function August() {
   return (
     <div className="august-container">
-    <h2 className="august-title">August</h2>
-    <h3 className="what-to-plant">Planting</h3>
-    <ul className="august-planting-list">
-      <li className="august-item-list">Grapes</li>
-    </ul>
-    <h3 className="what-to-harvest">Harvesting</h3>
-    <ul className="what-to-harvest">
-      <li className="august-harvesting-list">Wheat</li>
-    </ul>
-  </div>
-  )
+      <h2 className="august-title">August</h2>
+      <h3 className="what-to-plant">Planting (outdoors)</h3>
+      <ul className="outdoor-planting-list">
+        <li className="august-outdoor-item-list">Cabbage</li>
+        <li className="august-outdoor-item-list">Chinese Cabbage</li>
+        <li className="august-outdoor-item-list">Kale</li>
+        <li className="august-outdoor-item-list">Lettuce</li>
+        <li className="august-outdoor-item-list">Pak Choi</li>
+        <li className="august-outdoor-item-list">Radish</li>
+        <li className="august-outdoor-item-list">Rocket</li>
+        <li className="august-outdoor-item-list">Spinach</li>
+        <li className="august-outdoor-item-list">Spring Onion</li>
+      </ul>
+      <h3 className="what-to-harvest-title">Harvesting</h3>
+      <ul className="what-to-harvest">
+        <li className="august-harvesting-list">Artichoke (Globe)</li>
+        <li className="august-harvesting-list">Aubergine</li>
+        <li className="august-harvesting-list">Beetroot</li>
+        <li className="august-harvesting-list">Broccoli</li>
+        <li className="august-harvesting-list">Broad Bean</li>
+        <li className="august-harvesting-list">Brussels Sprout</li>
+        <li className="august-harvesting-list">Cabbage</li>
+        <li className="august-harvesting-list">Carrot</li>
+        <li className="august-harvesting-list">Cauliflower</li>
+        <li className="august-harvesting-list">Celeriac</li>
+        <li className="august-harvesting-list">Celery</li>
+        <li className="august-harvesting-list">Chard</li>
+        <li className="august-harvesting-list">Climbing Bean</li>
+        <li className="august-harvesting-list">Courgette</li>
+        <li className="august-harvesting-list">Cucumber</li>
+        <li className="august-harvesting-list">Dwarf Bean</li>
+        <li className="august-harvesting-list">Garlic, Kohl</li>
+        <li className="august-harvesting-list">Rabi</li>
+        <li className="august-harvesting-list">Leek</li>
+        <li className="august-harvesting-list">Lettuce</li>
+        <li className="august-harvesting-list">Marrow</li>
+        <li className="august-harvesting-list">Onion</li>
+        <li className="august-harvesting-list">Pak Choi</li>
+        <li className="august-harvesting-list">Pea</li>
+        <li className="august-harvesting-list">Pepper</li>
+        <li className="august-harvesting-list">Potato</li>
+        <li className="august-harvesting-list">Pumpkin</li>
+        <li className="august-harvesting-list">Radish</li>
+        <li className="august-harvesting-list">Rocket</li>
+        <li className="august-harvesting-list">Runner Bean</li>
+        <li className="august-harvesting-list">Samphire</li>
+        <li className="august-harvesting-list">Shallot</li>
+        <li className="august-harvesting-list">Spinach</li>
+        <li className="august-harvesting-list">Spring Onion</li>
+        <li className="august-harvesting-list">Squash</li>
+        <li className="august-harvesting-list">Sweetcorn</li>
+        <li className="august-harvesting-list">Tomato</li>
+        <li className="august-harvesting-list">Turnip</li>
+      </ul>
+    </div>
+  );
 }
