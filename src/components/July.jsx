3899f261678c8fc577023d3b848f4e6cb453c0b9
Dukewendo <dommct@yahoo.co.uk>
2023-10-22 22:51:50 +0100

    July content

--- src/components/July.jsx
+++ src/components/July.jsx
@@ -1,19 +1,62 @@
-import React from 'react';
+import React from "react";
 
 import "./monthstyles.css";
 
 export default function July() {
   return (
     <div className="july-container">
-    <h2 className="july-title">July</h2>
-    <h3 className="what-to-plant">Planting</h3>
-    <ul className="july-planting-list">
-      <li className="july-item-list">Grapes</li>
-    </ul>
-    <h3 className="what-to-harvest">Harvesting</h3>
-    <ul className="what-to-harvest">
-      <li className="july-harvesting-list">Wheat</li>
-    </ul>
-  </div>
-  )
+      <h2 className="july-title">July</h2>
+      <h3 className="what-to-plant">Planting (outdoors)</h3>
+      <ul className="outdoor-planting-list">
+        <li className="july-outdoor-item-list">Beetroot</li>
+        <li className="july-outdoor-item-list">Cabbage</li>
+        <li className="july-outdoor-item-list">Chard</li>
+        <li className="july-outdoor-item-list">Chinese Cabbage</li>
+        <li className="july-outdoor-item-list">Climbing Bean</li>
+        <li className="july-outdoor-item-list">Dwarf Bean</li>
+        <li className="july-outdoor-item-list">Kohl Rabi</li>
+        <li className="july-outdoor-item-list">Lettuce</li>
+        <li className="july-outdoor-item-list">Pak Choi</li>
+        <li className="july-outdoor-item-list">Radish</li>
+        <li className="july-outdoor-item-list">Rocket</li>
+        <li className="july-outdoor-item-list">Runner Bean</li>
+        <li className="july-outdoor-item-list">Spinach</li>
+        <li className="july-outdoor-item-list">Spring Onion</li>
+        <li className="july-outdoor-item-list">Swede</li>
+      </ul>
+      <h3 className="what-to-harvest-title">Harvesting</h3>
+      <ul className="what-to-harvest">
+        <li className="july-harvesting-list">Artichoke (Globe)</li>
+        <li className="july-harvesting-list">Beetroot</li>
+        <li className="july-harvesting-list">Broccoli</li>
+        <li className="july-harvesting-list">Broad Bean</li>
+        <li className="july-harvesting-list">Cabbage</li>
+        <li className="july-harvesting-list">Carrot</li>
+        <li className="july-harvesting-list">Cauliflower</li>
+        <li className="july-harvesting-list">Chard</li>
+        <li className="july-harvesting-list">Climbing Bean</li>
+        <li className="july-harvesting-list">Courgette</li>
+        <li className="july-harvesting-list">Cucumber</li>
+        <li className="july-harvesting-list">Dwarf Bean</li>
+        <li className="july-harvesting-list">Garlic</li>
+        <li className="july-harvesting-list">Khol Rabi</li>
+        <li className="july-harvesting-list">Leek</li>
+        <li className="july-harvesting-list">Lettuce</li>
+        <li className="july-harvesting-list">Marrow</li>
+        <li className="july-harvesting-list">Pak Choi</li>
+        <li className="july-harvesting-list">Pea</li>
+        <li className="july-harvesting-list">Pepper</li>
+        <li className="july-harvesting-list">Potato</li>
+        <li className="july-harvesting-list">Radish</li>
+        <li className="july-harvesting-list">Rocket</li>
+        <li className="july-harvesting-list">Runner Bean</li>
+        <li className="july-harvesting-list">Samphire</li>
+        <li className="july-harvesting-list">Spinach</li>
+        <li className="july-harvesting-list">Spring Onion</li>
+        <li className="july-harvesting-list">Squash</li>
+        <li className="july-harvesting-list">Tomato</li>
+        <li className="july-harvesting-list">Turnip</li>
+      </ul>
+    </div>
+  );
 }
