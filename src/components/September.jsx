60e694822f9f52c44c903ad10e35ada3814253cb
Dukewendo <dommct@yahoo.co.uk>
2023-10-22 23:28:36 +0100

    september content

--- src/components/September.jsx
+++ src/components/September.jsx
@@ -1,17 +1,40 @@
-import React from 'react'
+import React from "react";
+
+import "./monthstyles.css";
 
 export default function September() {
   return (
     <div className="september-container">
-    <h2 className="september-title">September</h2>
-    <h3 className="what-to-plant">Planting</h3>
-    <ul className="september-planting-list">
-      <li className="september-item-list">Grapes</li>
-    </ul>
-    <h3 className="what-to-harvest">Harvesting</h3>
-    <ul className="what-to-harvest">
-      <li className="september-harvesting-list">Wheat</li>
-    </ul>
-  </div>
-  )
+      <h2 className="september-title">September</h2>
+      <h3 className="what-to-plant">Planting (indoor)</h3>
+      <ul className="indoor-planting-list">
+        <li className="september-indoor-item-list"></li>
+        <li className="september-indoor-item-list"></li>
+        <li className="september-indoor-item-list"></li>
+        <li className="september-indoor-item-list"></li>
+        <li className="september-indoor-item-list"></li>
+        <li className="september-indoor-item-list"></li>
+        <li className="september-indoor-item-list"></li>
+      </ul>
+      <h3 className="what-to-plant">Planting (outdoors)</h3>
+      <ul className="outdoor-planting-list">
+        <li className="september-outdoor-item-list"></li>
+        <li className="september-outdoor-item-list"></li>
+      </ul>
+      <h3 className="what-to-harvest-title">Harvesting</h3>
+      <ul className="what-to-harvest">
+        <li className="september-harvesting-list"></li>
+        <li className="september-harvesting-list"></li>
+        <li className="september-harvesting-list"></li>
+        <li className="september-harvesting-list"></li>
+        <li className="september-harvesting-list"></li>
+        <li className="september-harvesting-list"></li>
+        <li className="september-harvesting-list"></li>
+        <li className="september-harvesting-list"></li>
+        <li className="september-harvesting-list"></li>
+        <li className="september-harvesting-list"></li>
+        <li className="september-harvesting-list"></li>
+      </ul>
+    </div>
+  );
 }
