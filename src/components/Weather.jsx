cc52d6c92b3cbb802cb6eeaed21c010a167721ca
Dukewendo <dommct@yahoo.co.uk>
2023-10-23 18:58:22 +0100

    setting up fetch request for weather component

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
-
-import './weather.css'
+import React, { useEffect } from "react";
 
 export default function Weather() {
+
+  const apiKey = 
+
+ useEffect(() => {
+  fetch("https://api.weatherbit.io/v2.0/forecast/daily?lat=54.57623&lon=-1.23483&key=API-KEY-HERE")
+ })
+
   return (
-    <div className='weather-container'>
-        <div className="week-container">
-          <h4>Weather forcast</h4>
-            <div className="monday">Mon</div>
-            <div className="tuesday">Tues</div>
-            <div className="wednesday">Wed</div>
-            <div className="thursday">Thurs</div>
-            <div className="friday">Fri</div>
-            <div className="saturday">Sat</div>
-            <div className="sunday">Sun</div>
-        </div>
+    <div className="week-weather-container">
+      <h4>This weeks weather</h4>
+      <div className="mon-weather">Mon</div>
+      <div className="tues-weather">Tues</div>
+      <div className="wed-weather">Wed</div>
+      <div className="thurs-weather">Thurs</div>
+      <div className="fri-weather">Fri</div>
+      <div className="sat-weather">Sat</div>
+      <div className="sun-weather">Sun</div>
     </div>
-  )
+  );
 }
