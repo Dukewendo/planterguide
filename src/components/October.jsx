89e781aba5019cef5bec9d15e531863e6d6fd3cf
Dukewendo <dommct@yahoo.co.uk>
2023-10-23 11:26:52 +0100

    october content

--- src/components/October.jsx
+++ src/components/October.jsx
@@ -1,17 +1,62 @@
-import React from 'react'
+import React from "react";
+
+import "./monthstyles.css";
 
 export default function October() {
   return (
     <div className="october-container">
-    <h2 className="october-title">October</h2>
-    <h3 className="what-to-plant">Planting</h3>
-    <ul className="october-planting-list">
-      <li className="october-item-list">Grapes</li>
-    </ul>
-    <h3 className="what-to-harvest">Harvesting</h3>
-    <ul className="what-to-harvest">
-      <li className="october-harvesting-list">Wheat</li>
-    </ul>
-  </div>
-  )
+      <h2 className="october-title">October</h2>
+      <h3 className="what-to-plant">Planting (indoor)</h3>
+      <ul className="indoor-planting-list">
+        <li className="october-indoor-item-list">Lettuce</li>
+      </ul>
+      <h3 className="what-to-plant">Planting (outdoors)</h3>
+      <ul className="outdoor-planting-list">
+        <li className="october-outdoor-item-list">Broad Bean</li>
+        <li className="october-outdoor-item-list">Pea</li>
+      </ul>
+      <h3 className="what-to-harvest-title">Harvesting</h3>
+      <ul className="what-to-harvest">
+        <li className="october-harvesting-list">Artichoke (Globe)</li>
+        <li className="october-harvesting-list">Artichoke (Jerusalem)</li>
+        <li className="october-harvesting-list">Aubergine</li>
+        <li className="october-harvesting-list">Beetroot</li>
+        <li className="october-harvesting-list">Broccoli</li>
+        <li className="october-harvesting-list">Brussels Sprout</li>
+        <li className="october-harvesting-list">Cabbage</li>
+        <li className="october-harvesting-list">Carrot</li>
+        <li className="october-harvesting-list">Cauliflower</li>
+        <li className="october-harvesting-list">Celeriac</li>
+        <li className="october-harvesting-list">Celery</li>
+        <li className="october-harvesting-list">Chard</li>
+        <li className="october-harvesting-list">Chicory</li>
+        <li className="october-harvesting-list">Chinese Cabbage</li>
+        <li className="october-harvesting-list">Climbing Bean</li>
+        <li className="october-harvesting-list">Courgette</li>
+        <li className="october-harvesting-list">Cucumber</li>
+        <li className="october-harvesting-item">Dwarf Bean</li>
+        <li className="october-harvesting-item">Kale</li>
+        <li className="october-harvesting-item">Kohl Rabi</li>
+        <li className="october-harvesting-item">Leek</li>
+        <li className="october-harvesting-item">Lettuce</li>
+        <li className="october-harvesting-item">Marrow</li>
+        <li className="october-harvesting-item">Pak Choi</li>
+        <li className="october-harvesting-item">Parsnip</li>
+        <li className="october-harvesting-item">Pepper</li>
+        <li className="october-harvesting-item">Potato</li>
+        <li className="october-harvesting-item">Pumpkin</li>
+        <li className="october-harvesting-item">Radish</li>
+        <li className="october-harvesting-item">Rocket</li>
+        <li className="october-harvesting-item">Runner Bean</li>
+        <li className="october-harvesting-item">Samphire</li>
+        <li className="october-harvesting-item">Shallot</li>
+        <li className="october-harvesting-item">Spinach</li>
+        <li className="october-harvesting-item">Spring Onion</li>
+        <li className="october-harvesting-item">Squash</li>
+        <li className="october-harvesting-item">Swede</li>
+        <li className="october-harvesting-item">Tomato</li>
+        <li className="october-harvesting-item">Turnip</li>
+      </ul>
+    </div>
+  );
 }
