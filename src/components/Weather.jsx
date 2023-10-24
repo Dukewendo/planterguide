21768d1d8151a8fe12fedfabf61e2520d13f8333
Dukewendo <dommct@yahoo.co.uk>
2023-10-24 12:52:27 +0100

    logic for API response

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,12 +1,40 @@
 import React, { useEffect } from "react";
 
 export default function Weather() {
+  useEffect(() => {
+    fetch(
+      "https://api.weatherbit.io/v2.0/forecast/daily?lat=54.57623&lon=-1.23483&key=API_KEY_HERE"
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Request failed with status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("data retrieved", data);
+        console.log(data.data[0].weather.description)
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
 
-  const apiKey = 
+    const days_of_the_week = [
+      "Sun",
+      "Mon",
+      "Tues",
+      "Wed",
+      "Thurs",
+      "Fri",
+      "Sat",
+    ];
 
- useEffect(() => {
-  fetch("https://api.weatherbit.io/v2.0/forecast/daily?lat=54.57623&lon=-1.23483&key=API-KEY-HERE")
- })
+    const now = new Date();
+    const day_today = now.getDay();
+    // console.log(
+    //   `Today is ${days_of_the_week[day_today]} and the forecast is ${data.data[0].weather.description}:`
+    //);
+  }, []);
 
   return (
     <div className="week-weather-container">
