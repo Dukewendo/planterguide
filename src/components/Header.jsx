56676dbcbaa0dd3229a076cf14f4457fd8a5a56b
Dukewendo <dommct@yahoo.co.uk>
2023-10-27 20:48:33 +0100

    minor css update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function header() {
   return (
     <div className="header-container">
       <ul className="header-list">
-        <h1 className="main-page-title">Uk Planting guide</h1>
+        <h2 className="main-page-title">Uk Planting guide</h2>
         <li className="header-list-item">Home</li>
         <li className="header-list-item">About</li>
       </ul>
